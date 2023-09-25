d624a6926df63bcd7ce4e53b659b40498437869f
nbiswajeet77 <nbiswajeet77@gmail.com>
2023-09-25 23:44:02 +0530

    api for chatting

--- agents/chatgpt.go
+++ agents/chatgpt.go
@@ -1,17 +1,62 @@
 package agents
 
 import (
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
 
 	"vartalabh.com/m/model"
 )
 
-func MakeChatGptCall(messages []model.Message) *model.Message {
-	apiKey := "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"
+func Chat(w http.ResponseWriter, r *http.Request) {
+	baseRole := "you are a mental health counsellor. Talk to user, ask repititive questions,keep the conversation going. Also ask the user how much progress he made based on the prompt provided."
+	messages := []model.Message{
+		{
+			Role:    "system",
+			Content: baseRole,
+		},
+	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%s %d %s", "*************Session ", i+1, " started*************\n")
+		for j := 0; j < 1000; j++ {
+			fmt.Printf("User: ")
+			in := bufio.NewReader(os.Stdin)
+			input, _ := in.ReadString('\n')
+			input = strings.TrimSpace(input)
+			if input == "exit" {
+				messages = append(messages, model.Message{
+					Role:    "user",
+					Content: "Summarise the entire conversation focusing on what problems the user faced, the summary is for a counsellor",
+				})
+				response := makeChatGptCall(messages)
+				fmt.Println(response.Content)
+				messages = []model.Message{
+					{
+						Role:    "system",
+						Content: baseRole + response.Content,
+					},
+				}
+				break
+			}
+			messages = append(messages, model.Message{
+				Role:    "user",
+				Content: input,
+			})
+			response := makeChatGptCall(messages)
+			fmt.Println(response.Content)
+			messages = append(messages, *response)
+		}
+	}
+}
+
+func makeChatGptCall(messages []model.Message) *model.Message {
+	apiKey := "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"
 	endpoint := "https://api.openai.com/v1/chat/completions"
 
 	reqBody := &model.GPT3Request{
--- main.go
+++ main.go
@@ -1,53 +1,17 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strings"
+	"log"
+	"net/http"
 
 	"vartalabh.com/m/agents"
-	"vartalabh.com/m/model"
 )
 
-func main() {
-	baseRole := "you are a mental health counsellor. Talk to user, ask repititive questions,keep the conversation going. Also ask the user how much progress he made based on the prompt provided."
-	messages := []model.Message{
-		{
-			Role:    "system",
-			Content: baseRole,
-		},
-	}
+func handleRequests() {
+	http.HandleFunc("/chat", agents.Chat)
+	log.Fatal(http.ListenAndServe(":10000", nil))
+}
 
-	for i := 0; i < 3; i++ {
-		fmt.Printf("%s %d %s", "*************Session ", i+1, " started*************\n")
-		for j := 0; j < 1000; j++ {
-			fmt.Printf("User: ")
-			in := bufio.NewReader(os.Stdin)
-			input, _ := in.ReadString('\n')
-			input = strings.TrimSpace(input)
-			if input == "exit" {
-				messages = append(messages, model.Message{
-					Role:    "user",
-					Content: "Summarise the entire conversation focusing on what problems the user faced, the summary is for a counsellor",
-				})
-				response := agents.MakeChatGptCall(messages)
-				fmt.Println(response.Content)
-				messages = []model.Message{
-					{
-						Role:    "system",
-						Content: baseRole + response.Content,
-					},
-				}
-				break
-			}
-			messages = append(messages, model.Message{
-				Role:    "user",
-				Content: input,
-			})
-			response := agents.MakeChatGptCall(messages)
-			fmt.Println(response.Content)
-			messages = append(messages, *response)
-		}
-	}
+func main() {
+	handleRequests()
 }
