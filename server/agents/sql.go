e8f855d5f256cc1988c3c6ccd5d19121b61eb78d
Vithik Shah <vithikshah@gmail.com>
2024-03-07 23:25:05 +0530

    fixes

--- server/agents/sql.go
+++ server/agents/sql.go
@@ -93,7 +93,7 @@ func FetchUserChats(userID string) ([]*model.ChatHistoryResponse, error) {
 		if err := json.Unmarshal(msg, &messages); err != nil {
 			return nil, err
 		}
-		if len(messages) > 1 {
+		if len(messages) > 2 {
 			chathistory = append(chathistory, &model.ChatHistoryResponse{
 				ChatId:    chatId,
 				Prompt:    prompt,
