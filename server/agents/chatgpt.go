e7cb2fb54dbb0ece4d0560d84d4c91aac3685bcb
Vithik Shah <vithikshah@gmail.com>
2024-03-09 20:27:21 +0530

    abc; g

--- server/agents/chatgpt.go
+++ server/agents/chatgpt.go
@@ -225,7 +225,7 @@ func makeChatGptCall(messages []model.Message) *model.Message {
 	endpoint := "https://api.openai.com/v1/chat/completions"
 
 	reqBody := &model.GPT3Request{
-		Model:    "gpt-3.5-turbo",
+		Model:    "gpt-4-0125-preview",
 		Messages: messages,
 	}
 
