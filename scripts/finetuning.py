4ef9b1a72b467f1e3e8fb850080870c7117bbd7d
Biswajit Nayak <biswajitnayak@Biswajits-Laptop.local>
2024-05-21 19:30:51 +0530

    commit

--- scripts/finetuning.py
+++ scripts/finetuning.py
@@ -1,5 +1,5 @@
 from openai import OpenAI
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="enter-key-here")
 
 client.fine_tuning.jobs.create(
   training_file="file-zTfK21gpZW1ZfVEMKJmT30Pb", 
